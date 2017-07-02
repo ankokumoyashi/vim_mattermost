f39efdee0ad3c508d996b2489b3469414aad37ce
ankokumoyashi <gen.4048.1105@gmail.com>
2017-07-02 15:32:04 +0900

    login request

--- login.py
+++ login.py
@@ -1,4 +1,11 @@
-{
-    "login_id":"ankokumoyashi",
-    "password":"ankokumoyashi"
-}
+import requests
+import json
+import pdb
+url = 'http://localhost:8065/api/v3/users/login'
+method = 'POST'
+headers = {"Content-Type": "application/json"}
+f = open('login.json')
+data = json.load(f)
+response = requests.post(url, data=json.dumps(data), headers=headers).json()
+pdb.set_trace()
+
